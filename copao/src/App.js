064af54cfc2becbeff082c5736da08c9a6b4f467
karthi228180 <karthikcl310@gmail.com>
2023-10-13 16:13:49 +0530

    karthik

--- copao/src/App.js
+++ copao/src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
 import Navbar from './components/Navigationbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Abouut from './pages/Karthik/Abouut';
+import Services from './pages/Karthik/Services';
+
+
+
+
+
+
+
 function App() {
   return (
-   <div>
+    <div>
 
-<Navbar/>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
 
-   </div>
+
+          <Route exact path='/Aboutus' element={<Abouut />} />
+          <Route exact path='/Services' element={<Services />} />
+
+
+
+        </Routes>
+      </BrowserRouter>
+
+    </div>
   );
 }
 
---
+++ copao/src/pages/Karthik/Services.js
@@ -0,0 +1,154 @@
+import React, { useEffect, useState } from 'react'
+import '../../css/Services.css'
+import Pic1 from '../../images/app-dev-1024x683.jpg'
+import Pic2 from '../../images/ui-ux-1024x683.jpg'
+import Pic3 from '../../images/8068473_63484-1-292x300.jpg'
+import Pic4 from '../../images/1_uDJTLivemR18proTkBsMEQ.jpg'
+import Pic5 from '../../images/digital-marketing-with-icons-business-people-300x234.jpg'
+import Pic6 from '../../images/soci-2-1024x792.jpg'
+import Pic7 from '../../images/html-system-website-concept-1024x683.jpg'
+import Avatar from 'react-avatar'
+import {IoIosArrowUp} from 'react-icons/io'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
+
+function Services() {
+    const [isVisible,setIsVisible] = useState(false)
+    const gotobtn = () => {
+        window.scrollTo({ top:0, left:0, behavior:'smooth'});
+    };
+    const listenToScroll = () => {
+            
+
+
+        let heightToHidden = 250;
+        const winScroll =  document.body.scrollTop || document.documentElement.scrollTop
+
+        if(winScroll > heightToHidden){
+            setIsVisible(true);
+        }else{
+            setIsVisible(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", listenToScroll);
+        return () => window.removeEventListener("scroll", listenToScroll)
+    },[]);
+
+
+
+    useEffect(() => {
+        Aos.init({duration:1500,delay: 100,});
+    },[]);
+
+  return (
+<>
+<div className='service-container'>
+   <div className='Service-1'>
+    <h1>Our Services</h1>
+    <p data-aos="fade-up" className='discription-1'>An overview of the services you offer and how they will benefit the visitor.</p>
+   </div>
+   <div className='Service-2'>
+    <div data-aos="fade-right" className='Item-1'>
+        <img className='pic1' src={Pic1} alt='app-dev-1024x683.jpg'/>
+        </div>
+        <div className='text-grp'>
+        <p data-aos="fade-left" className='text-1'>Android and IOS app development</p>
+        <p data-aos="fade-up" className='text-2'>Get the first-rate android and IOS app development services. We help you transform your business idea into a reality.</p>
+        <button data-aos="fade-up" type='submit'className='learn-btn-1'>Learn More</button>
+        </div>
+        <div className='text-grp'>
+        <p data-aos="fade-right" className='text-1'>UI & UX Design</p>
+        <p data-aos="fade-up" className='text-2'>We help you with the UI and UX designing services. UI and UX are actually two different professions.</p>
+        <button data-aos="fade-up" type='submit'className='learn-btn-1'>Learn More</button>
+        </div>
+        <div data-aos="fade-left" className='Item-1'>
+        <img className='pic3' src={Pic2} alt='ui-ux-1024x683.jpg'/>
+        </div>
+        <div data-aos="fade-right" className='Item-1'>
+        <img className='pic2' src={Pic3} alt='8068473_63484-1-292x300.jpg'/>
+        </div>
+        <div className='text-grp'>
+        <p data-aos="fade-left" className='text-1'>Graphical Design</p>
+        <p data-aos="fade-up" className='text-2'>Get the incomparable graphical design by us. Our graphic designers help you to promote, advertise your products.</p>
+        <button data-aos="fade-up" type='submit'className='learn-btn-1'>Learn More</button>
+        </div>
+        <div className='text-grp'>
+        <p data-aos="fade-right" className='text-1'>WordPress Websites</p>
+        <p data-aos="fade-up" className='text-2'>Here we have the WordPress developers who are experts in WordPress developing. WordPress is a free, open-source website creation platform.</p>
+        <button data-aos="fade-up" type='submit'className='learn-btn-1'>Learn More</button>
+        </div>
+        <div data-aos="fade-left" className='Item-1'>
+        <img className='pic3' src={Pic4} alt='1_uDJTLivemR18proTkBsMEQ.jpg'/>
+        </div>
+        <div data-aos="fade-right" className='Item-1'>
+        <img className='pic1' src={Pic5} alt='digital-marketing-with-icons-business-people-300x234.jpg'/>
+        </div>
+        <div className='text-grp'>
+        <p data-aos="fade-left" className='text-1'>Digital Marketing</p>
+        <p data-aos="fade-up" className='text-2'>Stay connected with your customers, promote sales and activities. We are here to help promote your sales. </p>
+        <button data-aos="fade-up" type='submit'className='learn-btn-1'>Learn More</button>
+        </div>
+        <div className='text-grp'>
+        <p data-aos="fade-right" className='text-1'>Social Media Managing</p>
+        <p data-aos="fade-up" className='text-2'>Looking for a social media manager? Relax…. We have an excellent team of social media managers. They help you to grow your business through social media. </p>
+        <button data-aos="fade-up" type='submit'className='learn-btn-1'>Learn More</button>
+        </div>
+        <div data-aos="fade-left" className='Item-1'>
+        <img className='pic3' src={Pic6} alt='soci-2-1024x792.jpg'/>
+        </div>
+        <div data-aos="fade-right" className='Item-1'>
+        <img className='pic1' src={Pic7} alt='html-system-website-concept-1024x683.jpg'/>
+        </div>
+        <div className='text-grp'>
+        <p data-aos="fade-left" className='text-1'>Web Application</p>
+        <p data-aos="fade-up" className='text-2'>Get the best web application services here….  Our web developers are experts in designing, developing and maintaining the web applications.</p>
+        <button data-aos="fade-up" type='submit'className='learn-btn-1'>Learn More</button>
+        </div>
+    </div>
+    <div className='testinomial'>
+        <div data-aos="zoom-in" className='testinom-head'>
+        <h1>
+             Client Testimonials
+        </h1>
+        </div>
+        <div data-aos="flip-left" className='Review '>
+        <p className='testonimal-1'>
+        “A testimonial from a client who benefited from your product or service. Testimonials can be a highly effective way of establishing credibility and increasing your company's reputation.”
+        </p>
+        <div className='client'>
+        <Avatar name='vikas D' size="80" round={true}  color='brown'/>
+        <h3>client Name</h3>
+        </div>
+        </div>
+        <div data-aos="flip-left" className='Review '>
+        <p className='testonimal-1'>
+        “A testimonial from a client who benefited from your product or service. Testimonials can be a highly effective way of establishing credibility and increasing your company's reputation.”
+        </p>
+        <div className='client'>
+        <Avatar name='Sid k' size="80" round={true}  color='brown'/>
+        <h3>client Name</h3>
+        </div>
+        </div>
+    </div>
+    <div className='Item-2'>
+        <h3>A Title to Turn the Visitor Into a Lead</h3>
+        <button data-aos="fade-up" type='submit'className='Contact-btn-1'>Contact Us</button>
+        <div className='Scroll-top'>
+        <p className='Item-text'>This is your chance to emphasize why the visitor should contact you right now.</p> 
+        {isVisible &&(
+        <IoIosArrowUp size={44} className='top-icon-2' onClick={gotobtn}/>
+        )}
+        </div>
+    </div>
+   </div>
+
+
+
+</>
+  )
+}
+
+export default Services
